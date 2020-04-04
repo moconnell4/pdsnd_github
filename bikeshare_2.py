2411b7c7f953ff32ee98e36c2c46542fa98e4a69
Michael O'Connell <moconnell4@gmail.com>
2020-04-04 14:52:43 -0500

    Remove hint from user input comment

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -18,7 +18,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington).
     i = True
     while i == True:
         city = input('Enter the city you would like data for (Chicago, Washington, New York City):\n').lower()
